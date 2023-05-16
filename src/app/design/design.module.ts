21475999740320d5ebc681d647f2452aa664f1f0
Jon Areas <areasjx@gmail.com>
2023-05-16 03:22:37 -0600

    feat: add `.git-blame-ignore-revs`

--- src/app/design/design.module.ts
+++ src/app/design/design.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DesignRoutingModule } from './design-routing.module';
 import { IconsComponent } from './icons/icons.component';
 import { LayoutComponent } from './layout/layout.component';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [IconsComponent, LayoutComponent],
-  imports: [CommonModule, DesignRoutingModule],
+  imports: [CommonModule, DesignRoutingModule, InputTextModule],
 })
 export class DesignModule {}
--- src/app/design/icons/icons.component.ts
+++ src/app/design/icons/icons.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IconService } from '../../../../projects/mds-light/src/lib/service/icons/icon.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'jx-icons',
@@ -14,52 +13,29 @@ export class IconsComponent implements OnInit {
 
   selectedIcon: any;
 
-  searchTerm: string | null;
-
-  constructor(
-    private iconService: IconService,
-    private router: Router,
-    private location: Location,
-    private activatedRoute: ActivatedRoute,
-  ) {}
+  constructor(private iconService: IconService, private router: Router) {}
 
   ngOnInit(): void {
-    this.activatedRoute.queryParamMap.subscribe(queryParams => {
-      this.searchTerm = queryParams.get('search');
-
-      this.iconService.getIcons().subscribe(data => {
-        const isNotDeprecated = (value: any) =>
-          value.icon.tags.indexOf('deprecate') === -1;
-        const isPresentInSearch = (value: any) =>
-          value.icon.tags.some((tag: string) =>
-            // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-            tag.includes(this.searchTerm as string),
-          );
-
-        data = data.filter(isNotDeprecated);
-
-        if (this.searchTerm) {
-          data = data.filter(isPresentInSearch);
-        }
-
-        const icons = data;
-        icons.sort((icon1, icon2) => {
-          if (icon1.properties.name < icon2.properties.name) return -1;
-          else if (icon1.properties.name > icon2.properties.name) return 1;
-          else return 0;
-        });
-
-        this.icons = icons;
-        this.filteredIcons = data;
+    this.iconService.getIcons().subscribe(data => {
+      data = data.filter(value => {
+        return value.icon.tags.indexOf('deprecate') === -1;
       });
+
+      const icons = data;
+      icons.sort((icon1, icon2) => {
+        if (icon1.properties.name < icon2.properties.name)
+          return -1;
+        else
+          return 0;
+      });
+
+      this.icons = icons;
+      this.filteredIcons = data;
     });
   }
 
   onFilter(event: Event): void {
     const searchText = (event.target as HTMLInputElement).value;
-
-    const queryParams = searchText ? { search: searchText } : undefined;
-    this.router.navigate([], { queryParams }).then(() => void {});
 
     if (!searchText) {
       this.filteredIcons = this.icons;
