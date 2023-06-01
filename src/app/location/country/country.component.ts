40a77aaa6a28c8c81e9845b3f0620e455190f95d
Jon <areasjx@gmail.com>
2023-06-01 16:40:24 -0600

    feat: delete country works
    Update country.component.ts

--- src/app/location/country/country.component.ts
+++ src/app/location/country/country.component.ts
@@ -1,27 +1,25 @@
 import { Component, ViewChild } from "@angular/core";
-import { CountryService } from './service/country.service';
-import { MessageService } from 'primeng/api';
-import { Country } from './model/country';
+import { CountryService } from "./service/country.service";
+import { MessageService } from "primeng/api";
+import { Country } from "./model/country";
 import { CountryDialogComponent } from "./components/country-dialog/country-dialog.component";
 import {
   MdsDeleteSingleComponent
 } from "../../../../projects/mds-light/src/lib/components/delete-single/mds-delete-single.component";
+import { catchError, switchMap, throwError } from "rxjs";
 
 @Component({
-  selector: 'jx-country',
-  templateUrl: './country.component.html',
-  styleUrls: ['./country.component.scss'],
+  selector: "jx-country",
+  templateUrl: "./country.component.html",
+  styleUrls: ["./country.component.scss"]
 })
 export class CountryComponent {
-
-  @ViewChild('countryDialog') countryDialog: CountryDialogComponent;
-  @ViewChild('deleteCountryDialog') deleteDialog: MdsDeleteSingleComponent;
-
+  @ViewChild("countryDialog") countryDialog: CountryDialogComponent;
+  @ViewChild("deleteCountryDialog") deleteDialog: MdsDeleteSingleComponent;
 
   deleteCountryDialog = false;
 
   deleteCountriesDialog = false;
-
 
   country: Country = {};
   // TODO : Handle selected countries
@@ -36,33 +34,59 @@ export class CountryComponent {
 
   constructor(
     private countryService: CountryService,
-    private messageService: MessageService,
-  ) {}
+    private messageService: MessageService
+  ) {
+  }
 
   handleSelectedCountriesChange(selectedCountries: Country[]): void {
     this.selectedCountries = selectedCountries;
   }
 
-  confirmDelete(itemName: string): void {
+  confirmDelete(): void {
     this.deleteCountryDialog = false;
-
-    this.messageService.add({
-      severity: 'success',
-      summary: 'Successful',
-      detail:  `${itemName} Deleted`,
-      life: 3_000,
-    });
+    this.countryService
+      .delete(<number>this.country.countryId)
+      .pipe(
+        switchMap(() => this.countryService.findAll()),
+        catchError(error => {
+          this.displayErrorMessage();
+          return throwError(error);
+        })
+      )
+      .subscribe(data => {
+        this.countryService.setCountryChange(data);
+        this.displayDeleteMessage(this.country.description);
+        this.deleteDialog.closeDialog();
+      });
     this.country = {};
+  }
+
+  private displayDeleteMessage(itemName?: string): void {
+    this.messageService.add({
+      severity: "success",
+      summary: "Successful",
+      detail: `${itemName || "Country"} Deleted`,
+      life: 3000
+    });
+  }
+
+  private displayErrorMessage(): void {
+    this.messageService.add({
+      severity: "error",
+      summary: "Error",
+      detail: "An error occurred while deleting the country.",
+      life: 3000
+    });
   }
 
   confirmDeleteSelected(): void {
     this.deleteCountriesDialog = false;
     // TODO : Delete all the selected countries
     this.messageService.add({
-      severity: 'success',
-      summary: 'Successful',
-      detail: 'Countries Deleted',
-      life: 3000,
+      severity: "success",
+      summary: "Successful",
+      detail: "Countries Deleted",
+      life: 3000
     });
     // TODO : Handle reset selected countries to empty
     // this.selectedCountries = [];
@@ -82,7 +106,6 @@ export class CountryComponent {
     this.country = country;
     this.deleteDialog.openDialog();
   }
-
 
   deleteSelectedCountries(): void {
     this.deleteCountriesDialog = true;
